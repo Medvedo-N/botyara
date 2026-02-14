1d95a7b83b800a5d97ece9c8eddac9620add930f
Medvedo-N <mirzoev.n.005@gmail.com>
2026-02-14 13:28:14 +0500

    Update sheets_client.py

--- app/sheets_client.py
+++ app/sheets_client.py
@@ -5,3 +5,14 @@ from googleapiclient.discovery import build
 def get_sheets_service():
     creds, _ = default(scopes=["https://www.googleapis.com/auth/spreadsheets"])
     return build("sheets", "v4", credentials=creds)
+
+
+def read_range(spreadsheet_id: str, a1_range: str):
+    service = get_sheets_service()
+    resp = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range=a1_range)
+        .execute()
+    )
+    return resp.get("values", [])
